98bf8d84dcc7613b11c09f563f1487af198d33e5
jonasPereira533 <jonaspereirasilvaa92@gmail.com>
2026-08-18 20:55:13 -0300

    ex02

--- exemplosCSharp/Program.cs
+++ exemplosCSharp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using exemplosCSharp.fixacao;
 
 
+/*    PRIMEIRO EXERCICIO 
 Console.WriteLine("Informe o salario atual: ");
 int salario = int.Parse(Console.ReadLine());
 
@@ -9,3 +10,15 @@ int aumento = int.Parse(Console.ReadLine());
 
 Console.WriteLine($"seu salario atual é: {salario}");
 Console.WriteLine($"Com o aumento seu salario será: {Calculadora.Somar(salario, aumento)}");
+*/
+
+
+Console.WriteLine("Informe o preço do produto: ");
+double preco = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Informe o percentual de desconto: ");
+double desconto = double.Parse(Console.ReadLine());
+
+Console.WriteLine
+    ($"O preço do produto com desconto é: {CalcularDesconto.Desconto(preco, desconto)}" + "\n" +
+    $"Você economizou {preco - CalcularDesconto.Desconto(preco, desconto):F2}");
---
+++ exemplosCSharp/fixacao/CalcularDesconto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exemplosCSharp.fixacao
+{
+    internal class CalcularDesconto
+    {
+        internal static double Desconto ( double valor, double percentual )
+        {
+            double valorFinal = valor - (valor * (percentual / 100));
+            return valorFinal;
+        }
+
+
+    }
+}
