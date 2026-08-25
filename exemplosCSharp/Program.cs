dc7e3555a89b9857d7196c944ac8784b196838e3
JONAS SILVA <2025101697@academicougb.com.br>
2026-08-25 19:52:45 -0300

    realizando um metodo para fazer teste

--- exemplosCSharp/Program.cs
+++ exemplosCSharp/Program.cs
@@ -10,10 +10,10 @@ int aumento = int.Parse(Console.ReadLine());
 
 Console.WriteLine($"seu salario atual é: {salario}");
 Console.WriteLine($"Com o aumento seu salario será: {Calculadora.Somar(salario, aumento)}");
-*/
 
 
-/* SEGUNDO EXERCICIO
+
+ SEGUNDO EXERCICIO
 Console.WriteLine("Informe o preço do produto: ");
 double preco = double.Parse(Console.ReadLine());
 
@@ -23,7 +23,7 @@ double desconto = double.Parse(Console.ReadLine());
 Console.WriteLine
     ($"O preço do produto com desconto é: {CalcularDesconto.Desconto(preco, desconto)}" + "\n" +
     $"Você economizou {preco - CalcularDesconto.Desconto(preco, desconto):F2}");
-*/
+
 
 
 Console.WriteLine("Informe a quantidade de dias que ficou com o carro: ");
@@ -39,3 +39,18 @@ int kmRodados = kmFinal - kmInicial;
 
 Console.WriteLine($"O valor a ser pago é: " +
     $"{AluguelCarro.CalcularAluguel(dias, kmRodados):F2}");
+*/
+
+decimal salario, valorAumento, novoSalario;
+float percentual = 10;
+
+Console.WriteLine("Informe o salario: ");
+salario = decimal.Parse(Console.ReadLine());
+Console.WriteLine("Informe o percentual de aumento: ");
+percentual = float.Parse(Console.ReadLine());
+
+CalculaAumento calc = new CalculaAumento();
+valorAumento = calc.AumentarSalario(salario, percentual);
+
+Console.WriteLine($"para o salário {salario} com o aumento de {percentual}%, o valor do aumento é {valorAumento} e o novo salário é {salario + valorAumento}");
+
---
+++ exemplosCSharp/fixacao/CalculaAumento.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exemplosCSharp.fixacao
+{
+    public class CalculaAumento
+    {
+        public decimal AumentarSalario (decimal salario, float percentual)
+        {
+            return salario * (decimal)percentual / 100; 
+        }
+    }
+}
