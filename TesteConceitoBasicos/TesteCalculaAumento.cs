bc507cb099ab8e354e4be305f0877138a6098ff2
JONAS SILVA <2025101697@academicougb.com.br>
2026-08-25 20:14:36 -0300

    primeiro teste bem sucedido

---
+++ TesteConceitoBasicos/TesteCalculaAumento.cs
@@ -0,0 +1,26 @@
+﻿using exemplosCSharp.fixacao;
+
+namespace TesteConceitoBasicos
+{
+    [TestClass]
+    public sealed class TesteCalculaAumento
+    {
+        [TestMethod]
+        public void TesteCalculaAumento0()
+        {
+            // cenario
+            decimal salario = 1000;
+            float percentual = 10;
+            decimal valorAumento;
+
+            CalculaAumento calc = new CalculaAumento();
+
+            // ação
+            valorAumento = calc.AumentarSalario(salario, percentual);
+
+            // verificação
+            Assert.AreEqual(valorAumento, 100);
+        }
+
+    }
+}
